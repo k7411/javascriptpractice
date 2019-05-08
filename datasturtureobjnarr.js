94d3976e465235465bd21783e84208e9cf300e62
Karan <kran@outlook.com>
2019-05-09 01:07:43 +0530

    added datastructureobjarr.js for data structure chapter

---
+++ datasturtureobjnarr.js
@@ -0,0 +1,44 @@
+/*let listOfNumber = [2,4,7,8];
+console.log(listOfNumber[2]);
+
+console.log(listOfNumber[0]);
+
+console.log(listOfNumber[3-2]);
+*/
+/* 
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+console.log(doh.toLowerCase());
+*/
+
+//Arary manupulation
+/*
+let seq = [1,2,3];
+seq.push(4,5,6);
+
+console.log(seq);
+seq.reverse();
+console.log(seq);
+
+
+seq.reverse();
+console.log(seq.slice(2,5));
+console.log(seq.pop());
+console.log(seq.pop());
+*/
+
+//objects
+
+
+let day1 ={
+    squirrel : false,
+    events : ["work","touched tree","pizza","running"]
+};
+
+console.log(day1.squirrel);
+console.log(day1.events[3]);
+console.log(day1.wolf);
+day1.wolf = false;
+console.log(day1.wolf);
+console.log(day1);
