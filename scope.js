2e9d16b509198e17083359d6e840b35ed9897e81
Karan <kran@outlook.com>
2019-05-09 00:42:05 +0530

    merge fixed

--- scope.js
+++ scope.js
@@ -3,15 +3,3 @@ if (true) {
 let y = 20;
 var z = 30;
 console.log(x + y + z);
-<<<<<<< HEAD
-// → 60
-}
-// y is not visible here
-console.log(x + z);
-// → 40
-=======
-
-}
-
-console.log(x + z);
->>>>>>> 40753e15149c8bafd7964b0cf126ea2c52b1638e
